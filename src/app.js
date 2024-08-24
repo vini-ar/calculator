e477480f8ebadd7b17a11e36ee7bc195adbd906c
Marcus Vinicyus <125088362+vini-ar@users.noreply.github.com>
2024-08-24 20:50:13 -0300

    feat: Add functionality for addition and subtraction operations in calculator
    - Added event listeners for addition and equal buttons.
    - Implemented logic to parse input expressions and perform addition and subtraction.
    - Updated display to show calculation results.
    - Added 'operate' function to handle different math operations.
    - Implemented 'sum' and 'subtraction' functions to perform arithmetic operations.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,50 @@
 const inputArea = document.querySelector('#calculator-display-current-input')
 const additionButton = document.querySelector('#additionButton')
+const subtractionbutton = document.querySelector('#subtractionbutton')
+const equalButton = document.querySelector('#equalButton')
+const display = document.querySelector(".calculator-display-history")
+
 
 additionButton.addEventListener('click', () => {
     inputArea.value += '+'
 })
 
-equalButton.addEventListener('click', sum)
-
-function sum() {
-    let stringList = inputArea.value.split("+")
-    integerList = stringList.map((item) => parseInt(item))
+equalButton.addEventListener('click', () => {
     
-    console.log(integerList)
+    let expression = inputArea.value
+    
+    let result;
+
+    let expressionFiltred = expression.split(/[+\-*/]/)
+    expressionFiltred = expressionFiltred.map(item => parseInt(item))
+
+    if (expression.includes("+")) {
+        result = operate("+", expressionFiltred[0], expressionFiltred[1])
+    }
+    else if (expression.includes("-")) {
+        result = operate("-", expressionFiltred[0], expressionFiltred[1])
+    }
+
+    let span = document.createElement("span")
+    span.textContent = result
+    display.appendChild(span)
+
+})
 
 
-    let sum = integerList.reduce((accumulator, item) => {
-        return accumulator + item
-    }) 
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return sum(num1, num2)
+    }
+    else if (operator === "-") {
+        return subtraction(num1, num2)
+    }
+}
 
-    console.log(sum)
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function subtraction(num1, num2) {
+    return num1 - num2
 }

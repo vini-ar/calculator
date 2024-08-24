c8b4b1370ba698da0cb65684dddae386a63a6941
Marcus Vinicyus <125088362+vini-ar@users.noreply.github.com>
2024-08-24 19:08:03 -0300

    Add basic addition functionality to calculator: update input area on button click and calculate sum on equal button

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log('Hello World')
+const inputArea = document.querySelector('#calculator-display-current-input')
+const additionButton = document.querySelector('#additionButton')
+
+additionButton.addEventListener('click', () => {
+    inputArea.value += '+'
+})
+
+equalButton.addEventListener('click', sum)
+
+function sum() {
+    let stringList = inputArea.value.split("+")
+    integerList = stringList.map((item) => parseInt(item))
+    
+    console.log(integerList)
+
+
+    let sum = integerList.reduce((accumulator, item) => {
+        return accumulator + item
+    }) 
+
+    console.log(sum)
+}

9181e0730b055c9363616aeeb13b0f97abc4b87e
Marcus Vinicyus <125088362+vini-ar@users.noreply.github.com>
2024-08-25 16:27:55 -0300

    finish calculator

--- src/app.js
+++ src/app.js
@@ -1,51 +1,53 @@
 const inputArea = document.querySelector('#calculator-display-current-input')
-const additionButton = document.querySelector('#additionButton')
-const subtractionbutton = document.querySelector('#subtractionbutton')
 const equalButton = document.querySelector('#equalButton')
 const display = document.querySelector(".calculator-display-history")
 const clearButton = document.querySelector("#clearButton")
-const numberList = document.querySelectorAll(".number")
+const digitButtonList = document.querySelectorAll(".number")
+const operatorButtonList = document.querySelectorAll(".operatorButton")
 
-additionButton.addEventListener('click', () => {
-    inputArea.value += '+'
-})
+let operatorList = ["+", '-', '/', '*'];
+let operator;
 
-clearButton.addEventListener('click', () => {
-    inputArea.value = ""
-})
-
-numberList.forEach(number => {
-    number.addEventListener("click", () => {
-        inputArea.value += number.textContent
+operatorButtonList.forEach(button => {
+    button.addEventListener("click", () => {
+        operator = operatorList.find((operator) => operator === button.value)
+        display.textContent += button.value
     })
 })
 
+digitButtonList.forEach(button => {
+    button.addEventListener("click", () => {
+        display.textContent += button.value
+    })
+})
+
+clearButton.addEventListener('click', () => {
+    display.textContent = ""
+})
+
 equalButton.addEventListener('click', () => {
-    
-    let expression = inputArea.value
-    
+
+    let expression = display.textContent
+
+    let expressionFiltred = expression.split(operator)
+    let numberList = expressionFiltred.map(item => parseInt(item))
+
     let result;
 
-    let expressionFiltred = expression.split(/[+\-*/]/)
-    let integerList = expressionFiltred.map(item => parseInt(item))
-
     if (expression.includes("+")) {
-        result = operate("+", integerList[0], integerList[1])
+        result = operate(operator, numberList[0], numberList[1])
     }
     else if (expression.includes("-")) {
-        result = operate("-", integerList[0], integerList[1])
+        result = operate(operator, numberList[0], numberList[1])
     }
     else if (expression.includes("*")) {
-        result = operate("*", integerList[0], integerList[1])
+        result = operate(operator, numberList[0], numberList[1])
     }
     else if (expression.includes("/")) {
-        result = operate("/", integerList[0], integerList[1])
+        result = operate(operator, numberList[0], numberList[1])
     }
 
-    let span = document.createElement("span")
-    span.textContent = result
-    display.appendChild(span)
-
+    display.textContent = result
 })
 
 
